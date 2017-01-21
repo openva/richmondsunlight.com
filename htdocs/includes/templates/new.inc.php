d573899949700ad5ddf5426e36795c605da2e699
Waldo Jaquith <waldo@jaquith.org>
2017-01-20 23:42:52 -0500

    Add a print stylesheet
    Per #102.

--- htdocs/includes/templates/new.inc.php
+++ htdocs/includes/templates/new.inc.php
@@ -5,6 +5,7 @@
 <meta name=viewport content="width=device-width, initial-scale=1">
 <title>%browser_title%</title>
 <link rel="stylesheet" href="/css/new/screen.css" type="text/css" media="screen" />
+<link rel="stylesheet" href="/css/new/print.css" type="text/css" media="print" />
 <link rel="stylesheet" href="/css/page-elements.css" type="text/css" media="screen" />
 <link rel="stylesheet" href="/css/prototip.css" type="text/css" media="screen" />
 <link rel="stylesheet" href="/css/jqueryui/custom/jquery-ui.css" type="text/css" media="screen">
