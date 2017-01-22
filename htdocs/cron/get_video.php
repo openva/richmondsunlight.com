936782325f92745e0dc25ab99fedf35367d3653a
Waldo Jaquith <waldo@jaquith.org>
2017-01-21 21:08:37 -0500

    Include Senate video
    Closes #107.

---
+++ htdocs/cron/get_video.php
@@ -0,0 +1,155 @@
+<?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+# INCLUDES
+# Include any files or libraries that are necessary for this specific
+# page to function.
+include_once('../includes/settings.inc.php');
+include_once('../includes/functions.inc.php');
+
+$sources = array(
+			'house' => 'http://virginia-house.granicus.com/VPodcast.php?view_id=3',
+			'senate' => 'http://virginia-senate.granicus.com/VPodcast.php?view_id=3'
+			);
+
+foreach ($sources as $chamber => $url)
+{
+
+	$cache_file = CACHE_DIR . 'video_rss_' . $chamber;
+
+	/*
+	 * Retrieve the RSS.
+	 */
+	$xml = get_content($url);
+
+	if ($xml === FALSE)
+	{
+		echo 'RSS could not be retrieved';
+		continue;
+	}
+
+	/*
+	 * Turn the XML into an object.
+	 */
+	$xml = simplexml_load_string($xml);
+
+	/*
+	 * Get the cached GUIDs.
+	 */
+	$guid_cache = array();
+	if (file_exists($cache_file))
+	{
+
+		$raw_cache = file_get_contents($cache_file);
+		if ($raw_cache !== FALSE)
+		{
+			$guid_cache = unserialize($raw_cache);
+		}
+
+	}
+
+	/*
+	 * Get all GUIDs from the XML.
+	 */
+	$guids = array();
+	foreach ($xml->channel->item as $item)
+	{
+		$guids[] = (string) $item->guid;
+	}
+
+	/*
+	 * See which GUIDs are new.
+	 */
+	$new_guids = array_diff($guids, $guid_cache);
+
+	if (count($new_guids) == 0)
+	{
+		continue;
+	}
+
+	/*
+	 * We'll keep our new videos in this array.
+	 */
+	$videos = array();
+
+	/*
+	 * Iterate through each new GUID, to find it in the XML.
+	 */
+	foreach ($new_guids as $guid)
+	{
+		
+		/*
+		 * Iterate through each XML item, to find this GUID.
+		 */
+		foreach ($xml->channel->item as $item)
+		{
+
+			if ($item->guid == $guid)
+			{
+
+				/*
+				 * Figure out the date of this video.
+				 */
+				$pos = strpos($item->title, '-');
+				if ($pos === FALSE)
+				{
+					continue;
+				}
+				$date = substr($item->title, 0, $pos);
+				$timestamp = strtotime($date);
+				$date = date('Ymd', $timestamp);
+
+				/*
+				 * Save this video date and URL.
+				 */
+				$videos[$date] = (string) $item->enclosure['url'];
+
+			}
+
+		}
+
+	}
+
+	/*
+	 * If we found any videos, retrieve them.
+	 */
+	if (count($videos) > 0)
+	{
+
+		/*
+		 * Take as long as necessary.
+		 */
+		set_time_limit(0);
+
+		foreach ($videos as $date => $url)
+		{
+
+			$filename = $_SERVER['DOCUMENT_ROOT'] . 'video/' . $chamber . '/floor/' . $date . '.mp4';
+			if (file_exists($filename) == TRUE)
+			{
+				continue;
+			}
+			$fp = fopen($filename, 'w+');
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_FILE, $fp);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+			curl_exec($ch); 
+			curl_close($ch);
+			fclose($fp);
+			echo '<p>Saved ' . $filename . '</p>';
+
+		}
+
+	}
+
+	/*
+	 * Write all item GUIDs back to the cache file.
+	 */ 
+	if (file_put_contents($cache_file, serialize($guids)) === FALSE)
+	{
+		echo 'Could not cache GUIDs.';
+	}
+
+}
