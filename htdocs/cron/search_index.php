bb883524f741a074785a63fd1ea0551017488e12
Waldo Jaquith <waldo@jaquith.org>
2017-08-30 22:38:43 -0400

    Fix filename, add tags

---
+++ htdocs/cron/search_index.php
@@ -0,0 +1,90 @@
+<?php
+
+/**
+ * Generate bulk JSON for indexing by Elasticsearch.
+ */
+
+/*
+ * Create our indexing directory, if it doesn't exist.
+ */
+$dir_name = 'search_index';
+if (file_exists($dir_name) === FALSE)
+{
+	if (!mkdir($dir_name, 0755))
+	{
+		$log->put('Could not create directory to index bills.', 7);
+		die();
+	}
+}
+chdir($dir_name);
+
+/*
+ * Get a list of what's already been exported.
+ */
+$files = scandir('.');
+
+/*
+ * Assemble the SQL query.
+ */
+$sql = 'SELECT sessions.year, bills.number, bills.catch_line, bills.summary,
+		bills.full_text, bills.interestingness,
+		(SELECT GROUP_CONCAT(tag) FROM tags WHERE bill_id=bills.id) AS tags
+		FROM bills
+		LEFT JOIN sessions
+			ON bills.session_id = sessions.id';
+
+/*
+ * If we already have files exported, then only get the bills that have been changed since
+ * the most recently-exported file.
+ */
+if (count($files) > 2)
+{
+	//$last_exported = '';
+	//$sql .= ' WHERE DATEDIFF(date_modified, ' . $last_exported . ') < 1';
+}
+
+/*
+ * Iterate through the results.
+ */
+$sth = $dbh->prepare($sql);
+$sth->execute();
+while ($bill = $sth->fetchObject())
+{
+
+	/*
+	 * Strip out newlines, which are a deal-breaker for Elasticsearch imports.
+	 */
+	$bill->summary = str_replace("\r", ' ', $bill->summary);
+	$bill->summary = str_replace("\n", ' ', $bill->summary);
+	$bill->full_text = str_replace("\r", ' ', $bill->full_text);
+	$bill->full_text = str_replace("\n", ' ', $bill->full_text);
+	$bill->summary = stripslashes($bill->summary);
+	$bill->full_text = stripslashes($bill->full_text);
+	$bill->catch_line = stripslashes($bill->catch_line);
+	$bill->summary = strip_tags($bill->summary);
+	$bill->full_text = strip_tags($bill->full_text);
+	$bill->catch_line = strip_tags($bill->catch_line);
+
+	/*
+	 * Set up the JSON preamble header, as instructinos for ElasticSearch.
+	 */
+	$header = array('index');
+	$header['index']['_index'] = 'rs';
+	$header['index']['_type'] = 'bills';
+	$header['index']['_id'] = $bill->year . '-' . $bill->number;
+
+	/*
+	 * Export as JSON.
+	 */
+	$json = json_encode($bill);
+
+	$filename = $bill->year . '-' . $bill->number . '.json';
+
+	$file_contents = json_encode($header) . "\n" . $json . "\n";
+
+	if (!file_put_contents($filename, $file_contents))
+	{
+		$log->put($bill->number . ' (' .$bill->year . ') could not be exported as JSON for indexing.', 3);
+	}
+
+}
