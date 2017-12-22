7a271d03c61897d97fa16188d2ad7c0cf87da695
Waldo Jaquith <waldo@jaquith.org>
2017-12-21 21:42:16 -0500

    Eliminate copyright entity

--- htdocs/legislator.php
+++ htdocs/legislator.php
@@ -171,7 +171,7 @@ if ($district_data !== FALSE && isset($district_data->region->center_lat) )
 			$( document ).ready(function() {
 				var district_map = L.map("district_map").setView([' . $district_data->region->center_lat . ', ' . $district_data->region->center_lon . '], 7);
 				L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-    				attribution: "Map data &copy; <a href=http://openstreetmap.org>OpenStreetMap</a> contributors, <a href=http://creativecommons.org/licenses/by-sa/2.0/>CC-BY-SA</a>, Imagery © <a href=http://mapbox.com>Mapbox</a>",
+    				attribution: "Map data © <a href=http://openstreetmap.org>OpenStreetMap</a> contributors, <a href=http://creativecommons.org/licenses/by-sa/2.0/>CC-BY-SA</a>, Imagery © <a href=http://mapbox.com>Mapbox</a>",
 				    maxZoom: 18,
 				    id: "' . MAPBOX_ID . '",
 				    accessToken: "' . MAPBOX_TOKEN . '"
