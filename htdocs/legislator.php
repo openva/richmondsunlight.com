d38dbdca01814917bfc9d2945dc3bb112ff80a3a
Waldo Jaquith <waldo@jaquith.org>
2017-12-21 23:38:56 -0500

    Fix Mapbox URL
    Closes #162.

--- htdocs/legislator.php
+++ htdocs/legislator.php
@@ -171,9 +171,9 @@ if ($district_data !== FALSE && isset($district_data->region->center_lat) )
 			$( document ).ready(function() {
 				var district_map = L.map("district_map").setView([' . $district_data->region->center_lat . ', ' . $district_data->region->center_lon . '], 7);
 				L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-    				attribution: "Map data © <a href=http://openstreetmap.org>OpenStreetMap</a> contributors, <a href=http://creativecommons.org/licenses/by-sa/2.0/>CC-BY-SA</a>, Imagery © <a href=http://mapbox.com>Mapbox</a>",
+    				attribution: "Map data &copy; <a href=http://openstreetmap.org>OpenStreetMap</a> contributors, <a href=http://creativecommons.org/licenses/by-sa/2.0/>CC-BY-SA</a>, Imagery © <a href=http://mapbox.com>Mapbox</a>",
 				    maxZoom: 18,
-				    id: "' . MAPBOX_ID . '",
+				    id: "mapbox.streets",
 				    accessToken: "' . MAPBOX_TOKEN . '"
 				}).addTo(district_map);
 				var district = L.polygon(' . json_encode($district_data->shape[0][0]) . ').addTo(district_map);
