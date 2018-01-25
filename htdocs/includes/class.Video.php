890eee433c47604c6227d9655dd273e7895a4680
Waldo Jaquith <waldo@jaquith.org>
2018-01-24 22:17:22 -0500

    Check for clips before creating them
    It turns out that it’s possible to yield *zero* clips, and that creates
    a bajillion errors. This stops that.

--- htdocs/includes/class.Video.php
+++ htdocs/includes/class.Video.php
@@ -721,6 +721,12 @@ class Video
 		# Get a list of all bill clips.
 		$this->clip_type = 'bills';
 		$this->index_clips();
+
+		# If there no clips were identified by index_clips(), then we're done here.
+		if (count($this->clips) == 0)
+		{
+			return FALSE;
+		}
 		
 		# Increment our counter.
 		$this->clip_count = $this->clip_count + count($this->clips);
