97ece337e30b6140a99b31279764faa3bbdd45dd
Waldo Jaquith <waldo@jaquith.org>
2018-01-27 20:10:32 -0500

    Work in progress for a staging deployment script
    Toward #141.

---
+++ deploy/staging-redeploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Clone a new copy of the source
+cd /vol/www/staging.richmondsunlight.com/htdocs
+rm -Rf *
+aws s3 cp s3://deploy.richmondsunlight.com/staging.zip staging.zip && unzip -o staging.zip
+
+# Duplicate the database.
+mysqldump -u {PDO_USERNAME} --password={PDO_PASSWORD} -h {PDO_SERVER} {MYSQL_DATABASE} | mysql -u {PDO_USERNAME} --password={PDO_PASSWORD} -h {PDO_SERVER} rs-staging
