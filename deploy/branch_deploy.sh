15a9bb45796cae8c2fd202d434c99e448647d755
Waldo Jaquith <waldo@jaquith.org>
2018-01-27 20:10:15 -0500

    We no longer need this functionality
    Toward #141.

--- deploy/branch_deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [ "$TRAVIS_BRANCH" = "master" ]
-then
-	echo "Configuring deployment to staging site"
-	sed -i -e "s|/vol/www/richmondsunlight.com|/vol/www/test.richmondsunlight.com|g" appspec.yml
-fi
