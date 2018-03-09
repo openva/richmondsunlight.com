094732c19a0e3303eb25310df4103d5ba1793358
Waldo Jaquith <waldo@jaquith.org>
2018-03-08 19:14:30 -0500

    Move JS into a vendor subdir

--- htdocs/includes/templates/new.inc.php
+++ htdocs/includes/templates/new.inc.php
@@ -22,10 +22,10 @@
 <![endif]-->
 <!--<link media="only screen and (max-device-width: 480px), only screen and (min-device-width: 560px) and (max-device-width: 1136px) and (-webkit-min-device-pixel-ratio: 2)"
 	href="/css/iphone.css" type="text/css" rel="stylesheet" />-->
-<script src="/js/jquery-1.7.1.min.js"></script>
+<script src="/js/vendor/jquery-1.7.1.min.js"></script>
 <script src="/js/jquery-ui-1.8.11.min.js"></script>
 <script src="/js/functions.js"></script>
-<script src="/js/jquery.qtip.min.js"></script>
+<script src="/js/vendor/jquery.qtip.min.js"></script>
 <?php
     # Include the below JavaScript, but only if the browser is IE. We do an Opera check because
     # Opera can include the text "MSIE" in its user agent string.
