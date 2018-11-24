4a1dff263aaf03f9eb491a2a0fbffedaaf3c8977
Waldo Jaquith <waldo@jaquith.org>
2018-11-23 22:20:31 -0500

    Add a new Tags class, autosuggest method
    Toward #171.

---
+++ htdocs/includes/class.Tags.php
@@ -0,0 +1,37 @@
+<?php
+
+class Tags
+{
+
+    # Take a fragment of a tag, get suggested autocompletions.
+    public function get_suggestions()
+    {
+        if (!isset($this->fragment))
+        {
+            return FALSE;
+        }
+
+        $database = new Database;
+        $database->connect_old();
+
+        $sql = 'SELECT tag AS text, COUNT(*) AS number
+        		FROM tags
+        		WHERE tag LIKE "' . $this->fragment . '%"
+        		GROUP BY tag
+        		HAVING number > 5
+        		ORDER BY number DESC
+        		LIMIT 5';
+        $result = mysql_query($sql);
+        if (mysql_num_rows($result) == 0)
+        {
+            return FALSE;
+        }
+        $tags = array();
+        while ($tag = mysql_fetch_array($result))
+        {
+            $tags[] = $tag['text'];
+        }
+
+        return $tags;
+    } // end get_suggestions()
+}
