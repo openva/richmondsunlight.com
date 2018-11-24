f56752266a8d734a8898d8f38cbf90d0d6eae859
Waldo Jaquith <waldo@jaquith.org>
2018-11-23 22:58:22 -0500

    Eliminate redundant JavaScript inclusions

--- htdocs/includes/templates/new.inc.php
+++ htdocs/includes/templates/new.inc.php
@@ -376,10 +376,7 @@
 			</footer>
 		</div>
 	</div>
-	<script src="/js/vendor/jquery/dist/jquery.min.js"></script>
-	<script src="/js/vendor/jquery-ui/jquery-ui.min.js"></script>
-	<script src="/js/vendor/qtip2/dist/jquery.qtip.min.js"></script>
-	<script src="/js/functions.js"></script>
+
 	<script pagespeed_no_defer="">
 		/* Create tabs. */
 		// Wait until the DOM has loaded before querying the document
