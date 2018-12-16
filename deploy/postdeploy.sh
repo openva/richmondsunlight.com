71dad2e5431e57848a149e887cca2ddbb8147e66
Waldo Jaquith <waldo@jaquith.org>
2018-12-15 21:12:02 -0500

    Expire the cached template on deploy
    Closes #265.

--- deploy/postdeploy.sh
+++ deploy/postdeploy.sh
@@ -3,3 +3,6 @@
 # Set permissions properly, since appspec.yml gets this wrong.
 chown -R ricsun:web /vol/www/richmondsunlight.com/
 chmod -R g+w /vol/www/richmondsunlight.com/
+
+# Expire the cached template (in case we've made changes to it).
+echo "delete template-new" | nc localhost 11211
