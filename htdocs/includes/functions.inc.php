9be07d26020a58f0776ef6f0df7675179197885d
Waldo Jaquith <waldo@jaquith.org>
2019-01-13 22:27:06 -0500

    Fix cURL timeout

--- htdocs/includes/functions.inc.php
+++ htdocs/includes/functions.inc.php
@@ -727,7 +727,10 @@ function get_content($url, $timeout=10)
 
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_HEADER, 0);
-    curl_setopt($ch, CURLOPT_TIMEOUT, $timeout);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 60);
+    curl_setopt($ch, CURLOPT_MAXREDIRS, 10);
+    curl_setopt($ch, CURLOPT_AUTOREFERER, true);
+    curl_setopt($ch, CURLOPT_ENCODING, 'gzip,deflate');
 
     ob_start();
 
