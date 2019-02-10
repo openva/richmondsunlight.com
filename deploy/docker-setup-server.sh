29a74023255dc87119cbdd1cb44e47ff85e8d744
Waldo Jaquith <waldo@jaquith.org>
2019-02-10 15:15:56 -0500

    Set htdocs as the webroot in Apache

--- deploy/docker-setup-server.sh
+++ deploy/docker-setup-server.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+# Make localhost the name of the host
 grep "ServerName localhost" /etc/apache2/apache2.conf
 if [ $? -ne 0 ]; then
     echo "ServerName localhost" >> /etc/apache2/apache2.conf
+fi
+
+# Make /var/www/htdocs the webroot
+grep "html" /etc/apache2/apache2.conf
+if [ $? -ne 0 ]; then
+    sed -i 's/html/htdocs/g' 000-default.conf
 fi
 
 # If the php.ini doesn't exist, create it.
@@ -10,4 +17,3 @@ if [ ! -f "/usr/local/etc/php/php.ini" ]; then
     cp /usr/local/etc/php/php.ini-development /usr/local/etc/php/php.ini
     echo "extension=memcached.so" >> /usr/local/etc/php/php.ini
 fi
-
