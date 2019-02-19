f8ece876262d968f31c0a12dc46f5b770162b7b3
Waldo Jaquith <waldo@jaquith.org>
2019-02-18 21:53:18 -0500

    Update deploy scripts to actually work
    As tuned in #123, this appears to actually work. The catch is that all of my testing was done with a single script that also retrieves the source, whereas for CodeDeploy, it's necessary to break it up into pre-deploy and post-deploy steps. CodeDeploy is also weirdly clueless as to whether this is a deploy that's running an update to an existing server or performing an installation to a fresh server. So I had to add conditionals, to avoid reconfiguring existing servers, and the best condition that I could think of was to see if the site already existed on the server.

--- deploy/postdeploy.sh
+++ deploy/postdeploy.sh
@@ -4,5 +4,24 @@
 chown -R ricsun:web /vol/www/richmondsunlight.com/
 chmod -R g+w /vol/www/richmondsunlight.com/
 
+# Set up Apache, if need be.
+SITE_SET_UP="$(sudo apache2ctl -S |grep richmondsunlight.com |wc -l)"
+if [ "SITE_SET_UP" -eq "0" ]; then
+
+    # Set up Apache
+    sudo cp deploy/virtualhost.txt /etc/apache2/sites-available/richmondsunlight.com.conf
+    sudo a2ensite richmondsunlight.com
+    sudo a2enmod headers expires rewrite http2
+    sudo systemctl reload apache2
+
+fi
+
+# Set the cache directory
+mkdir -p /vol/www/richmondsunlight.com/htdocs/cache
+sudo chgrp www-data /var/www/richmondsunlight.com/htdocs/cache
+
+# Install a certificate
+sudo certbot --apache -d test.richmondsunlight.com --non-interactive --agree-tos --email jaquith@gmail.com --redirect
+
 # Expire the cached template (in case we've made changes to it).
 echo "delete template-new" | nc localhost 11211
--- deploy/predeploy.sh
+++ deploy/predeploy.sh
@@ -1,22 +1,30 @@
 #!/bin/bash
 
-# Change the timezone to Eastern
-# sudo cp /usr/share/zoneinfo/US/Eastern /etc/localtime
+# If the site doesn't already exist, then this is a fresh server.
+SITE_SET_UP="$(sudo apache2ctl -S |grep richmondsunlight.com |wc -l)"
+if [ "SITE_SET_UP" -eq "0" ]; then
 
-# Remove all PHP packages (they may well be PHP 7)
-#sudo apt-get -y purge `dpkg -l | grep php| awk '{print $2}' |tr "\n" " "`
+    # Set the timezone to Eastern
+    sudo cp /usr/share/zoneinfo/US/Eastern /etc/localtime
 
-# Use repo for PHP 5.6.
-#sudo add-apt-repository -y ppa:ondrej/php
+    # Remove all PHP packages (they may well be PHP 7)
+    sudo apt-get -y purge `dpkg -l | grep php| awk '{print $2}' |tr "\n" " "`
 
-# Update the OS
-#sudo apt-get update
-#sudo apt-get -y upgrade
+    # Add the PHP 5 repo
+    sudo add-apt-repository -y ppa:ondrej/php
 
-# Install necessary packages.
-#sudo apt-get install -y php5.6-cli php5.6-mysql php5.6-curl php5.6-memcached composer zip ruby awscli acl
+    # Add the Certbot repo
+    sudo add-apt-repository -y ppa:certbot/certbot
 
-#cd ~
-#wget https://aws-codedeploy-us-east-1.s3.amazonaws.com/latest/install
-#chmod +x ./install
-#sudo ./install auto
+    # Install all packages.
+    sudo apt-get update
+    sudo DEBIAN_FRONTEND=noninteractive apt-get -y upgrade
+    sudo DEBIAN_FRONTEND=noninteractive apt-get install -y apache2 curl geoip-database git gzip unzip memcached openssl php5.6 php5.6-mysql mysql-client php5.6-curl php5.6-memcached  php5.6-mbstring php5.6-apc php5.6-mbstring php5.6-xml python python-pip s3cmd sphinxsearch wget awscli certbot python-certbot-apache 
+
+    # Install mod_pagespeed
+    wget https://dl-ssl.google.com/dl/linux/direct/mod-pagespeed-beta_current_amd64.deb
+    sudo dpkg -i mod-pagespeed-*.deb
+    sudo apt-get -f install
+    rm mod-pagespeed-*.deb
+    
+fi
