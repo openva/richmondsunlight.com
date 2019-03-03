da84cde2d439e3774af63502434fe89d08f50986
Waldo Jaquith <waldo@jaquith.org>
2019-03-02 20:25:44 -0500

    Fix Apache configuration
    The old setup wasn't working. This should do it.

--- deploy/docker-setup-server.sh
+++ deploy/docker-setup-server.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # Make localhost the name of the host
-grep "ServerName localhost" /etc/apache2/apache2.conf
-if [ $? -ne 0 ]; then
-    echo "ServerName localhost" >> /etc/apache2/apache2.conf
+grep "#ServerName www.example.com" /etc/apache2/sites-enabled/000-default.conf
+if [ $? -eq 0 ]; then
+    sed -i 's/#ServerName www.example.com/ServerName localhost/g' /etc/apache2/sites-enabled/000-default.conf
 fi
 
 # Make /var/www/htdocs the webroot
-grep "html" /etc/apache2/apache2.conf
-if [ $? -ne 0 ]; then
-    sed -i 's/html/htdocs/g' /etc/apache2/apache2.conf
+grep "DocumentRoot /var/www/html" /etc/apache2/sites-enabled/000-default.conf
+if [ $? -eq 0 ]; then
+    sed -i 's/html/htdocs/g' /etc/apache2/sites-enabled/000-default.conf
 fi
 
 # If the php.ini doesn't exist, create it.
