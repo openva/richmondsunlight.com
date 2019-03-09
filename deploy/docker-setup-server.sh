6737480771ba4e3c7929696d80aaaba8bb5990c8
Waldo Jaquith <waldo@jaquith.org>
2019-03-09 14:29:16 -0500

    Fix Docker server config
    I'm not sure what happened here, but these fixes will get things working again.

--- deploy/docker-setup-server.sh
+++ deploy/docker-setup-server.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # Make localhost the name of the host
-grep "ServerName localhost" /etc/apache2/apache2.conf
+grep "ServerName localhost" ls /etc/apache2/sites-available/000-default.conf
 if [ $? -ne 0 ]; then
-    echo "ServerName localhost" >> /etc/apache2/apache2.conf
+    sed -i 's/#ServerName www.example.com/ServerName localhost/g' /etc/apache2/sites-available/000-default.conf
 fi
 
 # Make /var/www/htdocs the webroot
-grep "html" /etc/apache2/apache2.conf
+grep "htdocs" /etc/apache2/sites-available/000-default.conf
 if [ $? -ne 0 ]; then
-    sed -i 's/html/htdocs/g' /etc/apache2/apache2.conf
+    sed -i 's/html/htdocs/g' /etc/apache2/sites-available/000-default.conf
 fi
 
 # If the php.ini doesn't exist, create it.
