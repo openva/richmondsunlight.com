17d2f816c4d0e77ef9cfe84197e52208efa6bb22
Waldo Jaquith <waldo@jaquith.org>
2019-03-12 21:50:55 -0400

    Add CodeDeploy installation to the deploy script

--- deploy/predeploy.sh
+++ deploy/predeploy.sh
@@ -19,12 +19,19 @@ if [ "SITE_SET_UP" -eq "0" ]; then
     # Install all packages.
     sudo apt-get update
     sudo DEBIAN_FRONTEND=noninteractive apt-get -y upgrade
-    sudo DEBIAN_FRONTEND=noninteractive apt-get install -y apache2 curl geoip-database git gzip unzip openssl php5.6 php5.6-mysql mysql-client php5.6-curl php5.6-mbstring php5.6-apc php5.6-mbstring php5.6-xml python python-pip s3cmd sphinxsearch wget awscli certbot python-certbot-apache 
+    sudo DEBIAN_FRONTEND=noninteractive apt-get install -y apache2 curl geoip-database git gzip unzip openssl php5.6 php5.6-mysql mysql-client php5.6-curl php5.6-mbstring php5.6-apc php5.6-mbstring php5.6-xml python python-pip s3cmd sphinxsearch wget awscli certbot python-certbot-apache
 
     # Install mod_pagespeed
     wget https://dl-ssl.google.com/dl/linux/direct/mod-pagespeed-beta_current_amd64.deb
     sudo dpkg -i mod-pagespeed-*.deb
     sudo apt-get -f install
     rm mod-pagespeed-*.deb
+
+    # Install Certbot
+    sudo apt-get install -y ruby
+    wget https://aws-codedeploy-us-east-1.s3.amazonaws.com/latest/install
+    chmod +x ./install
+    sudo ./install auto
+    rm install
     
 fi
