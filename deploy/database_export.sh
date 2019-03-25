b27f9b26f37e040c54ca14306791ac69885cf398
Waldo Jaquith <waldo@jaquith.org>
2019-03-24 22:11:23 -0400

    Add functionality to export test data
    Toward #209.

--- deploy/database_export.sh
+++ deploy/database_export.sh
@@ -11,10 +11,18 @@ STRUCTURE="bills bills_copatrons bills_full_text bills_places bills_section_numb
 ALL_CONTENTS="committees committee_members districts files representatives representatives_districts representatives_fundraising representatives_terms sessions"
 
 # All database tables that we want to export some contents of
-SOME_CONTENTS="bills bills_copatrons bills_full_text bills_places bills_section_numbers bills_status comments dockets meetings minutes polls representatives_votes tags video_clips video_index video_index_faces video_transcript votes"
+SOME_CONTENTS=(bills_copatrons bills_full_text bills_places bills_section_numbers bills_status bills_views comments dockets polls representatives_votes video_clips votes)
 
 # Export all of the structural data that we want
 mysqldump -d richmondsunlight --routines --triggers -u ricsun -p"$PASSWORD" --host "$HOST" "$STRUCTURE" > structure.sql
+# The ID of the bill to use to generate test data
+BILL_ID=46308
 
 # Export all of the tables for which we want complete contents
 mysqldump richmondsunlight --no-create-info -u ricsun -p"$PASSWORD" --host "$HOST" "$ALL_CONTENTS" > basic-contents.sql
+# Export selected contents from the remaining tables
+mysqldump richmondsunlight --no-create-info -u "$USERNAME" -p"$PASSWORD" --host "$HOST" bills --where "id=$BILL_ID" > test-records.sql 
+for TABLE in ${SOME_CONTENTS[*]}
+do
+    mysqldump richmondsunlight --no-create-info -u "$USERNAME" -p"$PASSWORD" --host "$HOST" "$TABLE" --where "bill_id=$BILL_ID" >> test-records.sql
+done
