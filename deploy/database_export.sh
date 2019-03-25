4dc1fa9c4237c955509a5854239038159fab9779
Waldo Jaquith <waldo@jaquith.org>
2019-03-24 22:11:37 -0400

    Improve export functionality

--- deploy/database_export.sh
+++ deploy/database_export.sh
@@ -13,13 +13,15 @@ ALL_CONTENTS="committees committee_members districts files representatives repre
 # All database tables that we want to export some contents of
 SOME_CONTENTS=(bills_copatrons bills_full_text bills_places bills_section_numbers bills_status bills_views comments dockets polls representatives_votes video_clips votes)
 
-# Export all of the structural data that we want
-mysqldump -d richmondsunlight --routines --triggers -u ricsun -p"$PASSWORD" --host "$HOST" "$STRUCTURE" > structure.sql
 # The ID of the bill to use to generate test data
 BILL_ID=46308
 
+# Export all of the structural data
+mysqldump richmondsunlight -d --routines --triggers -u "$USERNAME" -p"$PASSWORD" --host "$HOST" $STRUCTURE > structure.sql
+
 # Export all of the tables for which we want complete contents
-mysqldump richmondsunlight --no-create-info -u ricsun -p"$PASSWORD" --host "$HOST" "$ALL_CONTENTS" > basic-contents.sql
+mysqldump richmondsunlight --no-create-info -u "$USERNAME" -p"$PASSWORD" --host "$HOST" "$ALL_CONTENTS" > basic-contents.sql
+
 # Export selected contents from the remaining tables
 mysqldump richmondsunlight --no-create-info -u "$USERNAME" -p"$PASSWORD" --host "$HOST" bills --where "id=$BILL_ID" > test-records.sql 
 for TABLE in ${SOME_CONTENTS[*]}
