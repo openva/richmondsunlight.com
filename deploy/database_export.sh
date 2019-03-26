f32704b5c1140e46e1d30a0513f75d15f928fdb6
Waldo Jaquith <waldo@jaquith.org>
2019-03-25 21:52:06 -0400

    Add PII scrubbing
    Remove all email addresses and IP addresses from data dumps. Toward #209.

--- deploy/database_export.sh
+++ deploy/database_export.sh
@@ -10,8 +10,8 @@ STRUCTURE="bills bills_copatrons bills_full_text bills_places bills_section_numb
 # All database tables that we want to export the contents of
 ALL_CONTENTS="committees committee_members districts files representatives representatives_districts representatives_fundraising representatives_terms sessions"
 
-# All database tables that we want to export some contents of
-SOME_CONTENTS=(bills_copatrons bills_full_text bills_places bills_section_numbers bills_status bills_views comments dockets polls representatives_votes video_clips votes)
+# All database tables that we want to export some contents of, as test data
+SOME_CONTENTS=(bills_copatrons bills_full_text bills_places bills_section_numbers bills_status bills_views comments dockets polls video_clips votes)
 
 # The ID of the bill to use to generate test data
 BILL_ID=46308
@@ -21,14 +21,20 @@ cd `dirname $0`
 mkdir -p mysql
 
 # Export all of the structural data
-mysqldump richmondsunlight -d --routines --triggers -u "$USERNAME" -p"$PASSWORD" --host "$HOST" $STRUCTURE > mysql/structure.sql
+mysqldump richmondsunlight -d --routines --triggers -u "$USERNAME" -p"$PASSWORD" \
+    --host "$HOST" $STRUCTURE > mysql/structure.sql
 
 # Export all of the tables for which we want complete contents
-mysqldump richmondsunlight --no-create-info -u "$USERNAME" -p"$PASSWORD" --host "$HOST" $ALL_CONTENTS > mysql/basic-contents.sql
+mysqldump richmondsunlight --no-create-info -u "$USERNAME" -p"$PASSWORD" --host "$HOST" \
+    $ALL_CONTENTS > mysql/basic-contents.sql
 
 # Export selected contents from the remaining tables
-mysqldump richmondsunlight --no-create-info -u "$USERNAME" -p"$PASSWORD" --host "$HOST" bills --where "id=$BILL_ID" > mysql/test-records.sql 
+mysqldump richmondsunlight --no-create-info -u "$USERNAME" -p"$PASSWORD" --host "$HOST" bills \
+    --where "id=$BILL_ID" > mysql/test-records.sql 
 for TABLE in ${SOME_CONTENTS[*]}
 do
-    mysqldump richmondsunlight --no-create-info -u "$USERNAME" -p"$PASSWORD" --host "$HOST" "$TABLE" --where "bill_id=$BILL_ID" >> mysql/test-records.sql
+    mysqldump richmondsunlight --no-create-info -u "$USERNAME" -p"$PASSWORD" --host "$HOST" "$TABLE" \
+        --where "bill_id=$BILL_ID" |sed -E "s/'[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}'/192.168.0.1/g" \
+        |sed -E "s/\b[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}\b/example@example.com/g" \
+        >> mysql/test-records.sql
 done
