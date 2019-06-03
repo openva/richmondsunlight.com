7e5eb35e38095c1e5f13f6c12bc92fc8c69a36c9
Waldo Jaquith <waldo@jaquith.org>
2019-06-03 19:52:40 -0400

    Use the correct paths, usernames for deploy

--- deploy/postdeploy.sh
+++ deploy/postdeploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Set permissions properly, since appspec.yml gets this wrong.
-chown -R ricsun:web /vol/www/richmondsunlight.com/
-chmod -R g+w /vol/www/richmondsunlight.com/
+chown -R ubuntu:ubuntu /var/www/richmondsunlight.com/
+chmod -R g+w /var/www/richmondsunlight.com/
 
 # Set up Apache, if need be.
 SITE_SET_UP="$(sudo apache2ctl -S 2>&1 |grep -c richmondsunlight.com)"
@@ -18,8 +18,8 @@ if [ "$SITE_SET_UP" -eq "0" ]; then
     sudo certbot --apache -d richmondsunlight.com --non-interactive --agree-tos --email jaquith@gmail.com --redirect
 
     # Set the cache directory
-    mkdir -p /vol/www/richmondsunlight.com/htdocs/cache
-    sudo chgrp www-data /vol/www/richmondsunlight.com/htdocs/cache
+    mkdir -p /var/www/richmondsunlight.com/htdocs/cache
+    sudo chgrp www-data /var/www/richmondsunlight.com/htdocs/cache
 
 fi
 
