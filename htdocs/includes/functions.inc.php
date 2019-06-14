17f87467b2d386a1a72e067a05f805c79a8f01ac
Waldo Jaquith <waldo@jaquith.org>
2019-06-13 21:15:44 -0400

    Add nl2br helper functions
    WordPress uses a never-ending list of child functions to perform this simple action.

--- htdocs/includes/functions.inc.php
+++ htdocs/includes/functions.inc.php
@@ -878,6 +878,72 @@ function wp_replace_in_html_tags( $haystack, $replace_pairs ) {
     return $haystack;
 }
 
+/*
+ * A helper function for nl2p.
+ */
+function wp_html_split( $input ) {
+	return preg_split( get_html_split_regex(), $input, -1, PREG_SPLIT_DELIM_CAPTURE );
+}
+
+/*
+ * A helper function for nl2p.
+ */
+function _autop_newline_preservation_helper( $matches ) {
+	return str_replace( "\n", '<WPPreserveNewline />', $matches[0] );
+}
+
+/*
+ * A helper function for nl2p.
+ */
+function get_html_split_regex() {
+	static $regex;
+
+	if ( ! isset( $regex ) ) {
+		// phpcs:disable Squiz.Strings.ConcatenationSpacing.PaddingFound -- don't remove regex indentation
+		$comments =
+			'!'             // Start of comment, after the <.
+			. '(?:'         // Unroll the loop: Consume everything until --> is found.
+			.     '-(?!->)' // Dash not followed by end of comment.
+			.     '[^\-]*+' // Consume non-dashes.
+			. ')*+'         // Loop possessively.
+			. '(?:-->)?';   // End of comment. If not found, match all input.
+
+		$cdata =
+			'!\[CDATA\['    // Start of comment, after the <.
+			. '[^\]]*+'     // Consume non-].
+			. '(?:'         // Unroll the loop: Consume everything until ]]> is found.
+			.     '](?!]>)' // One ] not followed by end of comment.
+			.     '[^\]]*+' // Consume non-].
+			. ')*+'         // Loop possessively.
+			. '(?:]]>)?';   // End of comment. If not found, match all input.
+
+		$escaped =
+			'(?='             // Is the element escaped?
+			.    '!--'
+			. '|'
+			.    '!\[CDATA\['
+			. ')'
+			. '(?(?=!-)'      // If yes, which type?
+			.     $comments
+			. '|'
+			.     $cdata
+			. ')';
+
+		$regex =
+			'/('                // Capture the entire match.
+			.     '<'           // Find start of element.
+			.     '(?'          // Conditional expression follows.
+			.         $escaped  // Find end of escaped element.
+			.     '|'           // ... else ...
+			.         '[^>]*>?' // Find end of normal element.
+			.     ')'
+			. ')/';
+		// phpcs:enable
+	}
+
+	return $regex;
+}
+
 function login_form()
 {
     if (isset($_GET['return_uri']))
