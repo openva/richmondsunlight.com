bf38371f29b734564cc5d0fde02d17d5b8fd6cd5
Waldo Jaquith <waldo@jaquith.org>
2019-06-15 19:22:14 -0400

    All several front-end tests
    Toward #114.

--- deploy/tests/front-end.sh
+++ deploy/tests/front-end.sh
@@ -3,5 +3,13 @@
 # If any test fails, exit the test script
 set -e
 
-# Is the basic bill text being displayed?
-curl --silent "http://localhost:5000/bill/2019/sb1604/" |grep "<h1>Cruelty to animals"
+# Is basic bill metadata being displayed?
+curl --no-buffer --silent "http://localhost:5000/bill/2019/sb1604/" |grep -q "<h1>Cruelty to animals"
+
+# Is bill text being displayed?
+#curl --no-buffer --silent "http://localhost:5000/bill/2019/sb1604/fulltext" |grep -q ""
+
+# Is basic legislator information being displayed?
+curl --no-buffer --silent "http://localhost:5000/legislator/lradams/" |grep -q "<h1>Del. Les Adams"
+
+echo "Front-end tests passed"
