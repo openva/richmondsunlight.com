42100cb1653179400cbe272af5ce1593e9d4eeae
Waldo Jaquith <waldo@jaquith.org>
2019-11-28 22:54:03 -0500

    Continue work on the sidebar

--- htdocs/photosynthesis-sidebar.php
+++ htdocs/photosynthesis-sidebar.php
@@ -21,62 +21,80 @@ if (isset($_SESSION['portfolios']))
 
     $page_body .= <<<EOD
     <div id="portfolio-sidebar">
-        <h1><a class="handle">Your Bill Portfolio</a></h1>
+        <h5><a href="/photosynthesis/" class="handle">Your Bill Portfolio</a></h5>
         <div id="portfolio-list"></div>
-    <div>
+    </div>
 
     <!-- slider -->
+    <script src="https://cdn.rawgit.com/hawk-ip/jquery.tabSlideOut.js/v2.4/jquery.tabSlideOut.js"></script>
     <link rel="stylesheet" href="https://cdn.rawgit.com/hawk-ip/jquery.tabSlideOut.js/v2.4/jquery.tabSlideOut.css"> 
     <script src="https://use.fontawesome.com/2be9406092.js"></script>
-    <script src="https://cdn.rawgit.com/hawk-ip/jquery.tabSlideOut.js/v2.4/jquery.tabSlideOut.js"></script>
+
     <script>
-        $('#portfolio-sidebar').tabSlideOut( {'tabLocation':'bottom'} );
+        
+        $('#portfolio-sidebar').tabSlideOut({'tabLocation':'right','action':'hover'});
+
+        // List all bills in all portfolios
+        $.each( portfolios, function( index, portfolio_hash ) {
+            url = 'https://api.richmondsunlight.com/1.1/photosynthesis/' + portfolio_hash + '.json';
+            console.log(url);
+            $.getJSON(url, function(data) {
+                $.each( data.bills, function( index, bill ) {
+                    $( "#portfolio-list" ).append( '<div class="bill"><a href="' + bill.url + '">'
+                        + bill.number + '</a>: ' + bill.catch_line + '</div>');
+                });
+            });
+        });
+
     </script>
 
     <style>
         #portfolio-sidebar {
             width: 250px;
             min-height: 300px;
-            max-height: 600px;
+            max-height: 400px;
             padding: 5px;
-            right: 0px;
             z-index: 100;
-            background-color: white;
-            border: 5px solid black;
+            background-color: #f4eee5;
+            border: 5px solid #790806;
         }
-            #portfolio-sidebar .bill {
-                padding: 2px;
+            #portfolio-sidebar h5
+            {
+                font-size: 1em;
+                font-weight: normal;
+                padding-bottom: 0;
             }
-                #portfolio-sidebar .bill+.bill {
-                    margin-top: 1em;
+                #portfolio-sidebar h5 a.handle {
+                    font-family: Verdana, Lucida Grande, sans serif;
+                    background-color: #790806;
+                    color: white;
+                    border-radius: 10px 10px 0 0;
+                    text-decoration: none;
+                    font-weight: normal;
                 }
-                #portfolio-sidebar .bill.changed {
-                    background-color: yellow;
+                #portfolio-sidebar h5 a.handle:hover {
+                    text-decoration: none;
                 }
-                #portfolio-sidebar .bill .last-updated {
-                    text-transform: uppercase;
-                    font-size: .8em;
-                    color: red;
+                #portfolio-list {
+                    max-height: 385px;
+                    overflow: auto;
                 }
+                #portfolio-sidebar .bill {
+                    padding: 2px;
+                }
+                    #portfolio-sidebar .bill+.bill {
+                        margin-top: 1em;
+                    }
+                    #portfolio-sidebar .bill.changed {
+                        background-color: yellow;
+                    }
+                    #portfolio-sidebar .bill .last-updated {
+                        text-transform: uppercase;
+                        font-size: .8em;
+                        color: red;
+                    }
 
     </style>
-
-    <script>
-        var portfolios_bills = [];
-
-        // Create a list of all bills in all portfolios
-        $.each( portfolios, function( index, portfolio_hash ) {
-            url = 'https://api.richmondsunlight.com/1.1/photosynthesis/' + portfolio_hash + '.json';
-            $.getJSON(url, function(data) {
-                $.each( data.bills, function( index, bill ) {
-                    portfolios_bills.push(bill);
-                    $( "#portfolio-list" ).append( "<div class=bill><a href=" + bill.url + ">"
-                        + bill.number + "</a>: " + bill.catch_line + "</div>");
-                });
-            });
-        });
-
-    </script>
 EOD;
 }
 
