18f94f0bedc6741b26cb705406800a95794db25d
Waldo Jaquith <waldo@jaquith.org>
2019-12-02 22:00:44 -0500

    Change how the database export works
    The long list of tables is hitting mysqldump's hard limit on how long the list can be, which, based on a review of the source code for mysqldump, seems to assume will only list a single table, despite allowing multiple tables. So now, instead, mysqldump is run once per table.

--- deploy/database_export.sh
+++ deploy/database_export.sh
@@ -5,10 +5,10 @@ USERNAME="{PDO_USERNAME}"
 PASSWORD="{PDO_PASSWORD}"
 
 # All database tables that we want to export the structure of
-STRUCTURE="bills bills_copatrons bills_full_text bills_places bills_section_numbers bills_status bills_views blacklist chamber_status comments comments_subscriptions committees committee_members dashboard_bills dashboard_portfolios dashboard_user_data dashboard_watch_lists districts dockets files gazetteer meetings minutes polls representatives representatives_districts representatives_fundraising representatives_terms representatives_votes sessions tags users vacode video_clips video_index video_index_faces video_transcript votes"
+STRUCTURE=(bills bills_copatrons bills_full_text bills_places bills_section_numbers bills_status bills_views blacklist chamber_status comments comments_subscriptions committees committee_members dashboard_bills dashboard_portfolios dashboard_user_data dashboard_watch_lists districts dockets files gazetteer meetings minutes polls representatives representatives_districts representatives_fundraising representatives_terms representatives_votes sessions tags users vacode video_clips video_index video_index_faces video_transcript votes)
 
 # All database tables that we want to export the contents of
-ALL_CONTENTS="committees committee_members districts files representatives representatives_districts representatives_fundraising representatives_terms sessions"
+ALL_CONTENTS=(committees committee_members districts files representatives representatives_districts representatives_fundraising representatives_terms sessions)
 
 # All database tables that we want to export some contents of, as test data
 SOME_CONTENTS=(bills_copatrons bills_full_text bills_places bills_section_numbers bills_status bills_views comments dockets polls video_clips votes)
@@ -21,21 +21,27 @@ cd "$(dirname "$0")"
 mkdir -p mysql
 
 # Export all of the structural data
-mysqldump {MYSQL_DATABASE} -d --routines --triggers -u "$USERNAME" -p"$PASSWORD" \
-    --host "$HOST" --tables "$STRUCTURE" > mysql/structure.sql
-
-# Export all of the tables for which we want complete contents
-mysqldump {MYSQL_DATABASE} --no-create-info -u "$USERNAME" -p"$PASSWORD" --host "$HOST" \
-    --tables "$ALL_CONTENTS" > mysql/basic-contents.sql
-
-# Export selected contents from the remaining tables
-for BILL_ID in "${BILL_IDS[@]}"; do
-    mysqldump {MYSQL_DATABASE} --no-create-info -u "$USERNAME" -p"$PASSWORD" --host "$HOST" bills \
-        --where "id=$BILL_ID" > mysql/test-records.sql
+truncate --size 0 mysql/structure.sql
+for TABLE in ${STRUCTURE[*]}; do
+    mysqldump {MYSQL_DATABASE} -d --routines --triggers -u "$USERNAME" -p"$PASSWORD" \
+        --host "$HOST" --tables "$TABLE" >> mysql/structure.sql
 done
 
-for TABLE in ${SOME_CONTENTS[*]}
-do
+# Export all of the tables for which we want complete contents
+truncate --size 0 mysql/basic-contents.sql
+for TABLE in ${ALL_CONTENTS[*]}; do
+    mysqldump {MYSQL_DATABASE} --no-create-info -u "$USERNAME" -p"$PASSWORD" --host "$HOST" \
+        --tables "$TABLE" >> mysql/basic-contents.sql
+done
+
+# Export selected contents from the remaining tables
+truncate --size 0 mysql/test-records.sql
+for BILL_ID in "${BILL_IDS[@]}"; do
+    mysqldump {MYSQL_DATABASE} --no-create-info -u "$USERNAME" -p"$PASSWORD" --host "$HOST" bills \
+        --where "id=$BILL_ID" >> mysql/test-records.sql
+done
+
+for TABLE in ${SOME_CONTENTS[*]}; do
     for BILL_ID in "${BILL_IDS[@]}"; do
         # Genericize all IP addresses and email addresses, to maintain privacy.
         mysqldump {MYSQL_DATABASE} --no-create-info -u "$USERNAME" -p"$PASSWORD" --host "$HOST" "$TABLE" \
