4413bb54f0659325d9d75b0a3cfd3734e984d750
Waldo Jaquith <waldo@jaquith.org>
2021-11-06 22:48:19 -0400

    Install Matomo, optionally create a new Sphinx index

--- deploy/postdeploy.sh
+++ deploy/postdeploy.sh
@@ -20,6 +20,16 @@ chmod -R g+w "$SITE_PATH"
 chown -R www-data:www-data "$SITE_PATH"/htdocs/matomo
 chmod -R 777 "$SITE_PATH"/htdocs/matomo/tmp/
 
+# Set up Matomo, if need be.
+if [ ! -d "$SITE_PATH"/htdocs/matomo ]; then
+    cd /tmp || exit
+    wget https://builds.matomo.org/piwik.zip
+    unzip piwik.zip
+    sudo cp -r piwik "$SITE_PATH"/htdocs/matomo
+    sudo chown -R www-data:www-data "$SITE_PATH"/htdocs/matomo
+    sudo chmod -R 755 "$SITE_PATH"/htdocs/matomo
+fi
+
 # Set up Apache, if need be.
 SITE_SET_UP="$(sudo apache2ctl -S 2>&1 |grep -c " $SITE_URL ")"
 if [ "$SITE_SET_UP" -eq "0" ]; then
@@ -54,9 +64,16 @@ then
     # Copy over the Sphinx configuration, restart Sphinx
     sudo cp deploy/sphinx.conf /etc/sphinxsearch/sphinx.conf
     sudo /etc/init.d/sphinxsearch restart
-
-    # Index the database, continuing after logout, because it takes ~40 minutes to run
-    nohup sudo indexer --all --rotate > /dev/null 2>&1 &
+    
+    # If we have an existing index, update it
+    if [[ -f /var/lib/sphinxsearch/data/bills.sph ]]; then
+        # Reindex, continuing after logout, because it takes ~40 minutes to run
+        nohup sudo indexer --all --rotate > /dev/null 2>&1 &
+    # If there is no index, create a new one
+    else
+        nohup sudo indexer --all > /dev/null 2>&1 &
+    fi
+    
 fi
 
 # Expire the cached template (in case we've made changes to it)
