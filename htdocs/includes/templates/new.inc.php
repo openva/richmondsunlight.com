e325360722ea6ab27b5094de796b1f7f3521dc1b
Waldo Jaquith <waldo@jaquith.org>
2024-01-02 22:24:27 -0500

    Eliminate Matomo integration
    This just isn't worth the additional page load time, the (admittedly slim) reduction of privacy, or the technical overhead. Screw it: no more tracking.

--- htdocs/includes/templates/new.inc.php
+++ htdocs/includes/templates/new.inc.php
@@ -88,21 +88,6 @@
 ?>
 
 %html_head%
-<!-- Matomo -->
-<script type="text/javascript">
-  var _paq = window._paq || [];
-  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-  _paq.push(['trackPageView']);
-  _paq.push(['enableLinkTracking']);
-  (function() {
-    var u="//www.richmondsunlight.com/matomo/";
-    _paq.push(['setTrackerUrl', u+'matomo.php']);
-    _paq.push(['setSiteId', '1']);
-    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
-  })();
-</script>
-<!-- End Matomo Code -->
 </head>
 <body%body_tag%>
 	<div id="page-wrap">
