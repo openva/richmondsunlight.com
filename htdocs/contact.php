23e7db749e69d9ea54cc120830c6ab7d2e9e7647
Waldo Jaquith <waldo@jaquith.org>
2024-01-15 09:34:43 -0500

    Fix label content

--- htdocs/contact.php
+++ htdocs/contact.php
@@ -72,8 +72,8 @@ function show_form($form_data)
 		<div style="display: none;">
 			<input type="text" size="2" maxlength="2" name="form_data[zip]" id="message-zip" />
 			<label for="message-zip">Leave this field empty</label><br />
-			<label for="message-secret">Leave this field empty</label><br />
             <input type="text" size="10" maxlength="10" name="form_data[secret]" id="message-secret" value="' . time() * 2 . '" />
+			<label for="message-secret">Leave this field set to its default value</label><br />
 		</div>
 
 		<p><input type="submit" name="submit" value="Send Mail"></p>
