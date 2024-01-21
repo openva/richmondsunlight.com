9b63779684217071429625358e0975069da7ec72
Waldo Jaquith <waldo@jaquith.org>
2024-01-20 22:30:40 -0500

    Add E2E tests
    These aren't invoked anywhere yet in the CI/CD process, because none of this is of any use without standing up a Dockerized instance of the site to test again.
    
    [skip ci]

---
+++ deploy/tests/page-scan.php
@@ -0,0 +1,202 @@
+<?php
+
+$url_prefix = 'https://www.richmondsunlight.com';
+
+$failures = [];
+
+$pages =
+[
+    [
+        'url' => '/',
+        'http_status' => '200',
+        'string' => 'Welcome to Richmond Sunlight',
+    ],
+    [
+        'url' => '/bills/',
+        'http_status' => '200',
+        'string' => 'bills found',
+    ],
+    [
+        'url' => '/bills/2023/',
+        'http_status' => '200',
+        'string' => 'HB53',
+    ],
+    [
+        'url' => '/bills/2020/1/',
+        'http_status' => '200',
+        'string' => '488 bills found',
+    ],
+    [
+        'url' => '/bills/tags/education/',
+        'http_status' => '200',
+        'string' => 'bills found',
+    ],
+    [
+        'url' => '/bill/2024/hb0/',
+        'http_status' => '404',
+    ],
+    [
+        'url' => '/bill/2024/hb10223/',
+        'http_status' => '404',
+    ],
+    [
+        'url' => '/bill/2024/hb1/',
+        'http_status' => '200',
+        'string' => 'Minimum wage',
+    ],
+    [
+        'url' => '/bills/introduced/',
+        'http_status' => '200',
+        'string' => 'bills found',
+    ],
+    [
+        'url' => '/bills/activity/',
+        'http_status' => '200',
+        'string' => 'actions found',
+    ],
+    [
+        'url' => '/legislators/',
+        'http_status' => '200',
+        'string' => 'Charlottesville',
+    ],
+    [
+        'url' => '/legislator/rcdeeds/',
+        'http_status' => '200',
+        'string' => 'Sen. Creigh Deeds',
+    ],
+    [
+        'url' => '/legislator/jondoe/',
+        'http_status' => '404',
+    ],
+    [
+        'url' => '/legislator/rcdeeds/votes/2024/',
+        'http_status' => '200',
+        'string' => 'Senate Floor',
+    ],
+    [
+        'url' => '/legislator/rcdeeds/votes/2024.csv',
+        'http_status' => '200',
+        'string' => '',
+    ],
+    [
+        'url' => '/photosynthesis/portfolios/',
+        'http_status' => '200',
+        'string' => '',
+    ],
+    [
+        'url' => '/downloads/',
+        'http_status' => '200',
+        'string' => 'Metadata',
+    ],
+    [
+        'url' => '/schedule/2024/01/22/',
+        'http_status' => '200',
+        'string' => '',
+    ],
+    [
+        'url' => '/schedule/2024/01/32/',
+        'http_status' => '404',
+    ],
+    [
+        'url' => '/schedule/',
+        'http_status' => '200',
+        'string' => 'Schedule for',
+    ],
+    [
+        'url' => '/schedule/2023/01/27/',
+        'http_status' => '200',
+        'string' => 'CANCELLED',
+    ],
+    [
+        'url' => '/account/register/',
+        'http_status' => '200',
+        'string' => 'Create Your Account',
+    ],
+    [
+        'url' => '/search/',
+        'http_status' => '200',
+        'string' => '',
+    ],
+    [
+        'url' => '/search/?q=tax&year=2024',
+        'http_status' => '200',
+        'string' => 'HB261',
+    ],
+    [
+        'url' => '/search/?q=tax&year=',
+        'http_status' => '200',
+        'string' => 'HJ56',
+    ],
+    [
+        'url' => '/search/?q=nosuchresult',
+        'http_status' => '200',
+        'string' => '0 results found',
+    ],
+    [
+        'url' => '/committees/',
+        'http_status' => '200',
+        'string' => 'Appropriations',
+    ],
+    [
+        'url' => '/committee/house/appropriations/',
+        'http_status' => '200',
+        'string' => 'Transportation',
+    ],
+    [
+        'url' => '/committee/house/nosuchcommittee/',
+        'http_status' => '404',
+    ],
+    [
+        'url' => '/statistics/',
+        'http_status' => '200',
+        'string' => 'Bills Introduced Daily',
+    ],
+];
+
+/**
+ * Iterate through the list of pages, testing each
+ */
+foreach ($pages as $page) {
+    $ch = curl_init($url_prefix . $page['url']);
+    curl_setopt($ch, CURLOPT_HEADER, 1);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $content = curl_exec($ch);
+
+    if (curl_errno($ch)) {
+        echo 'cURL error: ' . curl_error($ch);
+        continue;
+    }
+
+    $http_status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+    if (!empty($page['http_status']) && $page['http_status'] != $http_status) {
+        $failures[] = ['page' => $page, 'error' => ['http_status' => $http_status]];
+        continue;
+    }
+
+    if (!empty($page['string']) && stristr($content, $page['string']) === false) {
+        $failures[] = ['page' => $page, 'error' => ['string' => false]];
+        continue;
+    }
+
+    curl_close($ch);
+}
+
+if (count($failures) > 0) {
+    echo 'Page scan failed with ' . count($failures) . ' errors' . ":\n\n";
+
+    foreach ($failures as $failure) {
+        echo '* ' . $failure['page']['url'] . ' returned ';
+        foreach ($failure['error'] as $key => $value) {
+            if ($key == 'string') {
+                $value = 'nothing that matched';
+            }
+            echo $value . ' for ' . $key . ' instead of ' . $failure['page'][$key];
+        }
+        echo "\n";
+    }
+    exit(1);
+}
+
+echo 'Tested ' . count($pages) . ' URLs, no errors found.' . "\n";
+exit(0);
