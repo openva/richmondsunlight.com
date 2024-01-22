8f772aa81c26cfd7c15462947b34629c497c7fc1
Waldo Jaquith <waldo@jaquith.org>
2024-01-22 17:10:19 -0500

    Update database export script

--- deploy/database_export.sh
+++ deploy/database_export.sh
@@ -5,10 +5,10 @@ USERNAME="{PDO_USERNAME}"
 PASSWORD="{PDO_PASSWORD}"
 
 # All database tables that we want to export the structure of
-STRUCTURE=(bills bills_copatrons bills_full_text bills_places bills_section_numbers bills_status bills_views blacklist chamber_status comments comments_subscriptions committees committee_members dashboard_bills dashboard_portfolios dashboard_user_data dashboard_watch_lists districts dockets files gazetteer meetings minutes polls representatives representatives_districts representatives_fundraising representatives_terms representatives_votes sessions tags users vacode video_clips video_index video_index_faces video_transcript votes)
+STRUCTURE=(bills bills_copatrons bills_full_text bills_places bills_places_test bills_section_numbers bills_status bills_views blacklist chamber_status comments comments_subscriptions committees committee_members dashboard_bills dashboard_portfolios dashboard_user_data dashboard_watch_lists districts dockets files gazetteer lobbyists meetings minutes petitions petition_signers polls representatives representatives_districts representatives_terms representatives_votes sessions tags users vacode video_clips video_index video_index_faces video_transcript votes)
 
 # All database tables that we want to export the contents of
-ALL_CONTENTS=(committees committee_members districts files representatives representatives_districts representatives_fundraising representatives_terms sessions)
+ALL_CONTENTS=(committees committee_members districts files representatives representatives_districts sessions)
 
 # All database tables that we want to export some contents of, as test data
 SOME_CONTENTS=(bills_copatrons bills_full_text bills_places bills_section_numbers bills_status bills_views comments dockets polls tags video_clips votes)
@@ -20,14 +20,14 @@ BILL_IDS=(45618 45663 46308 46058 45113 44355 44599 45136 45453 45811)
 cd "$(dirname "$0")"
 mkdir -p mysql
 
-# Export all of the structural data
+# Export the structural data
 truncate --size 0 mysql/structure.sql
 for TABLE in ${STRUCTURE[*]}; do
     mysqldump {MYSQL_DATABASE} -d --routines --triggers -u "$USERNAME" -p"$PASSWORD" \
         --host "$HOST" --tables "$TABLE" >> mysql/structure.sql
 done
 
-# Export all of the tables for which we want complete contents
+# Export the tables for which we want complete contents
 truncate --size 0 mysql/basic-contents.sql
 for TABLE in ${ALL_CONTENTS[*]}; do
     mysqldump {MYSQL_DATABASE} --no-create-info -u "$USERNAME" -p"$PASSWORD" --host "$HOST" \
