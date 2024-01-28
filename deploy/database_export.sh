26b8786e2c3fd92670d4bca5433ea5c15c90caed
Waldo Jaquith <waldo@jaquith.org>
2024-01-27 19:08:19 -0500

    Take the MySQL password off the command line

--- deploy/database_export.sh
+++ deploy/database_export.sh
@@ -4,6 +4,8 @@ HOST="{PDO_SERVER}"
 USERNAME="{PDO_USERNAME}"
 PASSWORD="{PDO_PASSWORD}"
 
+export MYSQL_PWD="$PASSWORD"
+
 # All database tables that we want to export the structure of
 STRUCTURE=(bills bills_copatrons bills_full_text bills_places bills_places_test bills_section_numbers bills_status bills_views blacklist chamber_status comments comments_subscriptions committees committee_members dashboard_bills dashboard_portfolios dashboard_user_data dashboard_watch_lists districts dockets files gazetteer lobbyists meetings minutes petitions petition_signers polls representatives representatives_districts representatives_terms representatives_votes sessions tags users vacode video_clips video_index video_index_faces video_transcript votes)
 
@@ -23,30 +25,33 @@ mkdir -p mysql
 # Export the structural data
 truncate --size 0 mysql/structure.sql
 for TABLE in ${STRUCTURE[*]}; do
-    mysqldump {MYSQL_DATABASE} -d --routines --triggers -u "$USERNAME" -p"$PASSWORD" \
+    mysqldump {MYSQL_DATABASE} -d --routines --triggers -u "$USERNAME" \
         --host "$HOST" --tables "$TABLE" >> mysql/structure.sql
 done
 
 # Export the tables for which we want complete contents
 truncate --size 0 mysql/basic-contents.sql
 for TABLE in ${ALL_CONTENTS[*]}; do
-    mysqldump {MYSQL_DATABASE} --no-create-info --lock-all-tables -u "$USERNAME" -p"$PASSWORD" \
+    mysqldump {MYSQL_DATABASE} --no-create-info --lock-all-tables -u "$USERNAME" \
         --host "$HOST" --tables "$TABLE" >> mysql/basic-contents.sql
 done
 
 # Export selected contents from the remaining tables
 truncate --size 0 mysql/test-records.sql
 for BILL_ID in "${BILL_IDS[@]}"; do
-    mysqldump {MYSQL_DATABASE} --no-create-info --lock-all-tables -u "$USERNAME" -p"$PASSWORD" \
+    mysqldump {MYSQL_DATABASE} --no-create-info --lock-all-tables -u "$USERNAME" \
         --host "$HOST" bills --where "id=$BILL_ID" >> mysql/test-records.sql
 done
 
 for TABLE in ${SOME_CONTENTS[*]}; do
     for BILL_ID in "${BILL_IDS[@]}"; do
         # Genericize all IP addresses and email addresses, to maintain privacy.
-        mysqldump {MYSQL_DATABASE} --no-create-info --lock-all-tables -u "$USERNAME" -p"$PASSWORD" --host "$HOST" "$TABLE" \
+        mysqldump {MYSQL_DATABASE} --no-create-info --lock-all-tables -u "$USERNAME" --host "$HOST" "$TABLE" \
             --where "bill_id=$BILL_ID" |perl -pe 's{[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}}{ sprintf "127.%01d.%01d.%01d", int(255*rand()), int(255*rand()), int(255*rand()) }ge' \
             |sed -E "s/\b[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}\b/example@example.com/g" \
             >> mysql/test-records.sql
     done
 done
+
+# Remove the environment variable, now that we're done with it
+unset MYSQL_PWD
