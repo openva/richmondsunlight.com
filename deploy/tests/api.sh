c78ddb99add8a3da618944aa56cd22e69e5fd2a5
Waldo Jaquith <waldo@jaquith.org>
2025-06-02 16:47:16 -0400

    Display body of error, instead of header

--- deploy/tests/api.sh
+++ deploy/tests/api.sh
@@ -39,7 +39,7 @@ else
 fi
 
 # DEBUG: Display the body of this response
-curl --silent --include http://localhost:5001/1.1/legislator/rcdeeds.json
+curl http://localhost:5001/1.1/legislator/rcdeeds.json
 
 # If any tests failed, have this script return that failure
 if [ "$ERRORED" == true ]; then
