c4db1a9c70633c34886995983cd66d1924b8c7f9
Waldo Jaquith <waldo@jaquith.org>
2025-08-20 23:10:19 -0400

    Replace S3 domain with a custom one
    This allows us to put the files behind CloudFront.

--- htdocs/admin/video/orphaned-chyrons.php
+++ htdocs/admin/video/orphaned-chyrons.php
@@ -78,7 +78,7 @@ if (count($_POST) == 0) {
     while ($chyron = mysqli_fetch_array($result)) {
         $chyron['url'] = str_replace(
             '/video/',
-            'http://s3.amazonaws.com/video.richmondsunlight.com/',
+            'http://video.richmondsunlight.com/',
             $chyron['url']
         );
 
