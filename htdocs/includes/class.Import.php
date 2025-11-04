adf8f7e94dd2bf2397e127e00b2783848107158e
Waldo Jaquith <waldo@jaquith.org>
2025-11-03 20:44:04 -0500

    Add more error-checking

--- htdocs/includes/class.Import.php
+++ htdocs/includes/class.Import.php
@@ -1270,9 +1270,11 @@ class Import
 						FROM sessions
 						WHERE date_started > now()';
                 $stmt = $GLOBALS['db']->prepare($sql);
-                $stmt->execute();
-                $session = $stmt->fetch(PDO::FETCH_OBJ);
-                if (count($session) > 0) {
+                $session = false;
+                if ($stmt && $stmt->execute()) {
+                    $session = $stmt->fetch(PDO::FETCH_OBJ);
+                }
+                if (!empty($session) && !empty($session->date_started)) {
                     $legislator['date_started'] = $session->date_started;
                 }
 
