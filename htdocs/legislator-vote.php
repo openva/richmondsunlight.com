aa43ba447722740eb9fa5940df45f518b65ff9ff
Waldo Jaquith <waldo@jaquith.org>
2025-11-30 11:30:33 -0500

    Experiment with different page output code
    This may be a better footer. It'll avoid warnings about unset variables.

--- htdocs/legislator-vote.php
+++ htdocs/legislator-vote.php
@@ -135,8 +135,9 @@ if (mysqli_num_rows($result) > 0) {
 
 # OUTPUT THE PAGE
 $page = new Page();
-$page->page_title = $page_title;
-$page->page_body = $page_body;
-$page->page_sidebar = $page_sidebar;
-$page->site_section = $site_section;
+foreach (array('page_title', 'page_body', 'page_sidebar', 'site_section') as $prop) {
+    if (isset(${$prop})) {
+        $page->{$prop} = ${$prop};
+    }
+}
 $page->process();
