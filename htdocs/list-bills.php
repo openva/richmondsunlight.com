1771df0645e9e53ac530e7d29f02d9931c7794ad
Waldo Jaquith <waldo@jaquith.org>
2025-11-30 19:41:05 -0500

    Add 2025 and 2026 links

--- htdocs/list-bills.php
+++ htdocs/list-bills.php
@@ -286,6 +286,8 @@ if (!empty($year)) {
 			<li><a href="/bills/2022/">2022</a></li>
 			<li><a href="/bills/2023/">2023</a></li>
 			<li><a href="/bills/2024/">2024</a></li>
+			<li><a href="/bills/2025/">2025</a></li>
+			<li><a href="/bills/2026/">2026</a></li>
 		</ul>
 
 		<p style="margin-top: 1em;">View bills that:</p>
