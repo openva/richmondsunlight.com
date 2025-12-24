e9fffb276f0056e21bdf3bedef38da7a9bc56fd5
Waldo Jaquith <waldo@jaquith.org>
2025-12-24 17:54:46 -0500

    Set up a Memcached instance in Docker
    This will allow sessions to be stored. Closes #863.

--- deploy/settings-docker.inc.php
+++ deploy/settings-docker.inc.php
@@ -40,8 +40,8 @@ define('MYSQL_DATABASE', 'richmondsunlight');
 define('API_URL', 'http://api/');
 
 # Specify how to connect to Memcached.
-define('MEMCACHED_SERVER', '');
-define('MEMCACHED_PORT', '');
+define('MEMCACHED_SERVER', 'memcached');
+define('MEMCACHED_PORT', '11211');
 
 # The House Speaker's IDs. This is used in update_vote.php to translate votes credited to
 # "H0000," which bizarrely indicates the speaker, to that legislator's ID, and in
