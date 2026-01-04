ab135720cd87df331c88dc896ad163662a95df16
Waldo Jaquith <waldo@jaquith.org>
2026-01-03 23:49:25 -0500

    Add persistent datbase connections
    #788

--- htdocs/includes/class.Database.php
+++ htdocs/includes/class.Database.php
@@ -33,9 +33,16 @@ class Database
         }
 
         /*
-         * Connect
+         * Connect with persistent connection and optimized settings
          */
-        $this->db = new PDO(PDO_DSN, PDO_USERNAME, PDO_PASSWORD);
+        $options = [
+            PDO::ATTR_PERSISTENT => true,
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES => false,
+            PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8mb4 COLLATE utf8mb4_unicode_ci"
+        ];
+        $this->db = new PDO(PDO_DSN, PDO_USERNAME, PDO_PASSWORD, $options);
 
         if ($this->db !== false) {
             $GLOBALS['db_pdo'] = $this->db;
@@ -145,5 +152,4 @@ class Database
         else {
             return false;
         }
-    }
 }
