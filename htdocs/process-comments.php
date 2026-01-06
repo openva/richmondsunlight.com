ffdbde709111bb60a4a8e92bc141dac4beadf620
Waldo Jaquith <waldo@jaquith.org>
2026-01-06 17:18:58 -0500

    Fix bad duplicate-comment checking
    Toward #869.

--- htdocs/process-comments.php
+++ htdocs/process-comments.php
@@ -216,7 +216,7 @@ foreach ($comment_words AS $word)
 # Make sure that this person hasn't posted in the past 5 seconds.
 $sql = 'SELECT id
 		FROM comments
-		WHERE (name="' . $comment['email'] . '" OR ip="' . $_SERVER['REMOTE_ADDR'] . '")
+		WHERE (email="' . $comment['email'] . '" OR ip="' . $_SERVER['REMOTE_ADDR'] . '")
 		AND (TIMESTAMPDIFF(SECOND, date_created, now()) < 5)';
 $result = mysqli_query($GLOBALS['db'], $sql);
 if (mysqli_num_rows($result) > 0) {
@@ -229,7 +229,7 @@ if (mysqli_num_rows($result) > 0) {
 # Make sure that this person hasn't posted too many times recently.
 $sql = 'SELECT *
 		FROM comments
-		WHERE (name="' . $comment['email'] . '" OR ip="' . $_SERVER['REMOTE_ADDR'] . '")
+		WHERE (email="' . $comment['email'] . '" OR ip="' . $_SERVER['REMOTE_ADDR'] . '")
 		AND (TIMESTAMPDIFF(MINUTE, date_created, now()) < 5)';
 $result = mysqli_query($GLOBALS['db'], $sql);
 if (mysqli_num_rows($result) > 10) {
@@ -242,7 +242,7 @@ if (mysqli_num_rows($result) > 10) {
 # Make sure that this person hasn't posted this precise same comment within the past hour.
 $sql = 'SELECT id
 		FROM comments
-		WHERE (name="' . $comment['email'] . '" OR ip="' . $_SERVER['REMOTE_ADDR'] . '")
+		WHERE (email="' . $comment['email'] . '" OR ip="' . $_SERVER['REMOTE_ADDR'] . '")
 		AND (TIMESTAMPDIFF(MINUTE, date_created, now()) < 60)
 		AND comment="' . $comment['comment'] . '"';
 $result = mysqli_query($GLOBALS['db'], $sql);
