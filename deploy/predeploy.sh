ebecb3181972c05a79114764050a41cb7b77e339
Waldo Jaquith <waldo@jaquith.org>
2026-01-07 21:00:29 -0500

    Install PHP-GD
    #800

--- deploy/predeploy.sh
+++ deploy/predeploy.sh
@@ -34,7 +34,7 @@ if [ "$SITE_SET_UP" -eq "0" ]; then
     sudo DEBIAN_FRONTEND=noninteractive apt -y upgrade
     sudo DEBIAN_FRONTEND=noninteractive apt install -y apache2 curl geoip-database git gzip \
     unzip openssl mysql-client memcached redis \
-    php php-mysql php-curl php-mbstring php-xml php-memcached php-redis \
+    php php-mysql php-curl php-mbstring php-xml php-memcached php-redis php-gd \
     python python-pip s3cmd sphinxsearch wget awscli certbot \
     python3-certbot-apache yarn
 
