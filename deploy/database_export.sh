ecac3af9caf58c24c404c07295a3841000d33d7d
Waldo Jaquith <waldo@jaquith.org>
2026-02-12 23:03:07 -0500

    Slim down exports, moving some to new file
    Move a bunch of more detailed records out of the GitHub-tracked SQL files, and into a file that is meant to be downloaded and stored locally. This will allow more robust testing locally, without continuing to balloon the size of the Git repo.

--- deploy/database_export.sh
+++ deploy/database_export.sh
@@ -12,10 +12,13 @@ export MYSQL_PWD="$PASSWORD"
 STRUCTURE=(bills bills_copatrons bills_full_text bills_places bills_section_numbers bills_status bills_status_narratives bills_views blacklist chamber_status comments comments_subscriptions committees committee_members dashboard_bills dashboard_portfolios dashboard_user_data dashboard_watch_lists districts dockets files fiscal_impact_statements gazetteer logs meetings minutes polls people representatives representatives_votes sessions tags terms users vacode video_clips video_index video_index_faces video_transcript votes)
 
 # All database tables that we want to export all contents of
-ALL_CONTENTS=(committees committee_members districts files people representatives sessions terms)
+ALL_CONTENTS=(committees committee_members districts people representatives sessions terms)
 
 # All database tables that we want to export some contents of, as test data
-SOME_CONTENTS=(bills_copatrons bills_full_text bills_places bills_section_numbers bills_status bills_status_narratives bills_views comments dockets fiscal_impact_statements polls tags video_clips votes)
+SOME_CONTENTS=(bills_copatrons bills_full_text bills_places bills_section_numbers bills_status bills_status_narratives bills_views comments dockets files fiscal_impact_statements polls tags votes)
+
+# Database tables that we want separate contents of, as local-only test data
+EXTENDED_CONTENTS=(video_index video_clips video_transcript)
 
 # The file IDs of videos and minutes that we want to export all the data for.
 VIDEO_CONTENTS=(14798)
@@ -32,6 +35,10 @@ VIDEO_CONTENTS=(14798)
 BILL_IDS=(
     77039 74613 72911 76873 74093 73894 73446 77034 72883 74728 73202 77430 72884 74117 73805 77318
     72954 73556 77399 72980 76972 76995 73362 74500 74148 76483 78711 78827 72932 73272 72955 78195
+)
+
+# A longer list of bill IDs, to have more test data.
+EXTENDED_BILL_IDS=(
     73118 79224 77600 78269 74670 73984 73286 76777 73960 77557 75107 74616 74490 73030 76924 78905
     76912 73053 74513 77733 73112 78556 78552 76706 74071 74585 77462 74875 76741 79465 77771 79881
     78258 77302 77668 74452 78892 79024 78235 73480 79928 77053 73896 73230 72902 78115 79529 74045
@@ -56,12 +63,33 @@ ALL_CONTENTS_LIST=$(printf "%s " "${ALL_CONTENTS[@]}")
 mysqldump --no-create-info --skip-lock-tables -u "$USERNAME" \
     --host "$HOST" {MYSQL_DATABASE} $ALL_CONTENTS_LIST > mysql/basic-contents.sql
 
-# Export selected contents from the remaining tables
+# Export selected contents from the other tables
 truncate --size 0 mysql/test-records.sql
-for BILL_ID in "${BILL_IDS[@]}"; do
+for BILL_ID in "${EXTENDED_BILL_IDS[@]}"; do
     mysqldump {MYSQL_DATABASE} --no-create-info --skip-lock-tables -u "$USERNAME" \
         --host "$HOST" bills --where "id=$BILL_ID" >> mysql/test-records.sql
 done
+
+for TABLE in "${SOME_CONTENTS[@]}"; do
+    for BILL_ID in "${EXTENDED_BILL_IDS[@]}"; do
+        # Genericize all IP addresses and email addresses, to maintain privacy.
+        mysqldump {MYSQL_DATABASE} --no-create-info --skip-lock-tables \
+            -u "$USERNAME" --host "$HOST" "$TABLE" \
+            --where "bill_id=$BILL_ID" |perl -pe 's{[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}}{ sprintf "127.%01d.%01d.%01d", int(255*rand()), int(255*rand()), int(255*rand()) }ge' \
+            |sed -E "s/\b[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}\b/example@example.com/g" \
+            >> mysql/test-records.sql
+    done
+done
+
+# Export selected contents for local-only testing
+truncate --size 0 mysql/local-test-records.sql
+for BILL_ID in "${BILL_IDS[@]}"; do
+    mysqldump {MYSQL_DATABASE} --no-create-info --skip-lock-tables -u "$USERNAME" \
+        --host "$HOST" bills --where "id=$BILL_ID" >> local-mysql/test-records.sql
+done
+
+# Append SOME_CONTENTS to EXTENDED_CONTENTS
+EXTENDED_CONTENTS+=("${SOME_CONTENTS[@]}")
 
 for TABLE in "${SOME_CONTENTS[@]}"; do
     for BILL_ID in "${BILL_IDS[@]}"; do
@@ -70,7 +98,7 @@ for TABLE in "${SOME_CONTENTS[@]}"; do
             -u "$USERNAME" --host "$HOST" "$TABLE" \
             --where "bill_id=$BILL_ID" |perl -pe 's{[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}}{ sprintf "127.%01d.%01d.%01d", int(255*rand()), int(255*rand()), int(255*rand()) }ge' \
             |sed -E "s/\b[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}\b/example@example.com/g" \
-            >> mysql/test-records.sql
+            >> mysql/local-test-records.sql
     done
 done
 
