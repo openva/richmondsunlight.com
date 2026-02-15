0ce848718715be020bb14aa3d6f12bb003f0585b
Waldo Jaquith <waldo@jaquith.org>
2026-02-14 23:01:39 -0500

    We need 2025 meetings data

--- deploy/database_export.sh
+++ deploy/database_export.sh
@@ -109,6 +109,10 @@ for FILE_ID in "${FILE_IDS[@]}"; do
         --host "$HOST" video_transcript --where "file_id=$FILE_ID" >> mysql/test-records.sql
 done
 
+# Meetings from the 2025 session
+mysqldump {MYSQL_DATABASE} --no-create-info --skip-lock-tables -u "$USERNAME" \
+    --host "$HOST" meetings --where "session_id=31" >> mysql/test-records.sql
+
 for TABLE in "${SOME_CONTENTS[@]}"; do
     for BILL_ID in "${EXTENDED_BILL_IDS[@]}"; do
         # Genericize all IP addresses and email addresses, to maintain privacy.
