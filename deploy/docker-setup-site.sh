c293f80314c96835d23dc491fac141a1657d6600
Waldo Jaquith <waldo@jaquith.org>
2026-02-16 22:41:19 -0500

    Add more tests
    - Logging in returns a cookie
    - Logged-in user can see their account profile
    - Committee page lists bills
    - Docket listings include bills
    - Code section API queries work
    - Spam comments are discarded
    - Blank comments are rejected
    - Value poll votes return a 302
    - CSV vote data URL returns a 200
    - Admin page returns a 401
    - Admin page returns a 200 for an admin user

--- deploy/docker-setup-site.sh
+++ deploy/docker-setup-site.sh
@@ -52,6 +52,10 @@ cd htdocs/js/vendor; yarn build; cd ../../..
 # Move over the settings file.
 cp deploy/settings-docker.inc.php htdocs/includes/settings.inc.php
 
+# Create a test admin password for the HTTP-Basic-protected /admin/ directory.
+# The password is "password123"; tests rely on this credential.
+htpasswd -bc htdocs/admin/.users admin password123
+
 # Note: Test users are now loaded as part of the initial database.sql file,
 # not via load-test-users.sh, to ensure consistency with the database initialization
 
