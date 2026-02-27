08ac83f46af72a4fbc169226459e5d0d663cf38f
Waldo Jaquith <waldo@jaquith.org>
2026-02-27 18:33:30 -0500

    Update ancient API docs

--- htdocs/about-api.php
+++ htdocs/about-api.php
@@ -15,44 +15,45 @@
     include_once 'vendor/autoload.php';
 
     # PAGE METADATA
-    $page_title = 'API v1.0';
+    $page_title = 'API v1.1';
     $site_section = 'about';
 
     # SIDEBAR CONTENT
     $page_sidebar = <<<EOD
 
-	<h3>What is “JSON”?</h3>
+	<h3>What is "JSON"?</h3>
 	<div class="box">
-		<p>JavaScript Object Notion is a lightweight, simplified XML-like format for storing and
-		exchanging data. It’s really great for pulling data across the internet from one server to
-		use on another. It’s supported by every programming language under the sun, nearly all of
-		which can retrieve JSON and turn it an object of data. For more information,
-		<a href="http://en.wikipedia.org/wiki/JSON">read the JSON entry on Wikipedia</a>.</p>
+		<p>JSON is a lightweight format for exchanging data. It's supported by every
+		programming language and is ideal for retrieving structured data from a server.
+		For more information, <a href="https://en.wikipedia.org/wiki/JSON">read the JSON
+		entry on Wikipedia</a>.</p>
 	</div>
 
 	<h3>How can I put this API to work?</h3>
 	<div class="box">
-		<p>In PHP, you’d use <a href="http://php.net/json_decode">json_decode</a>.In Perl, one of
-		the <a href="http://search.cpan.org/search?query=JSON">JSON modules</a>. In Python, <a
-		href="http://docs.python.org/library/json.html">json</a>. In Ruby, <a
-		href="http://flori.github.com/json/">JSON</a>. In jQuery, <a
-		href="http://api.jquery.com/jQuery.getJSON/">getJSON</a>.
-		<a href="http://www.factsandpeople.com/facts-mainmenu-5/26-html-and-javascript/89-jquery-ajax-json-and-php">A
-		brief tutorial</a> might be illustrative.</p>
+		<p>In PHP, use <a href="https://www.php.net/json_decode">json_decode</a>. In Python,
+		use the built-in <a href="https://docs.python.org/3/library/json.html">json</a>
+		module. In JavaScript, use <a href="https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API">fetch</a>
+		or any HTTP library.</p>
 	</div>
 
-	<h3>I Still Don’t Understand</h3>
+	<h3>I Still Don't Understand</h3>
 	<div class="box">
-		<p>Try out our <a href="/downloads/">downloads section</a>! It offers much of the data
-		provided via the API, but as single downloads in common file formats.</p>
+		<p>Try our <a href="/downloads/">downloads section</a>, which offers much of the
+		same data as single-file downloads in common formats.</p>
 	</div>
 
-	<h3>Cache!</h3>
+	<h3>Please Cache!</h3>
 	<div class="box">
-		<p>We must urge you to cache the data that you pull from Richmond Sunlight, rather than just
-		serving it up live to your visitors. Richmond Sunlight lacks the server power to feed live
-		data to third party websites, so when our system comes its knees, we’ll begin by cutting off
-		your server’s access.</p>
+		<p>Please cache the data you retrieve from Richmond Sunlight rather than serving it
+		live to your visitors. When our servers come under strain, we will begin cutting off
+		access from high-volume third-party consumers.</p>
+	</div>
+
+	<h3>Machine-Readable Spec</h3>
+	<div class="box">
+		<p>A full <a href="https://api.richmondsunlight.com/openapi.yaml">OpenAPI 3.0
+		specification</a> is available for programmatic use.</p>
 	</div>
 
 EOD;
@@ -60,147 +61,236 @@ EOD;
     # PAGE CONTENT
     $page_body = <<<EOD
 
-				<p>The v1.0 release of Richmond Sunlight’s API provides a series of JSON-based web
-				services. (It also supports JSONP and its associated callback function.) The
-				following methods are available:</p>
+	<p>The Richmond Sunlight API v1.1 provides JSON access to Virginia legislative data.
+	All endpoints are served from <code>https://api.richmondsunlight.com/1.1/</code>.
+	All responses are JSON. The following endpoints are available:</p>
 
-				<ul>
-					<li><a href="#bill">Bill</a></li>
-					<li><a href="#legislator">Legislator</a></li>
-					<li><a href="#code-section">Affected Section of Code</a></li>
-					<li><a href="#photosynthesis">Photosynthesis Portfolio</a></li>
-				</ul>
-
-				<h2 id="bills">Retrieve a list of bills.</h2>
-
-				<h3>Example URI</h3>
-				<p>http://api.richmondsunlight.com/1.0/bills/2011.json</p>
-
-				<h3>Instructions</h3>
-				<p>Replace the year (“2011”) with the year for which you want a listing of all
-				bills.</p>
-
-				<h3>Returns</h3>
-				<ul>
-					<li>number</li>
-					<li>chamber</li>
-					<li>date introduced</li>
-					<li>status</li>
-					<li>outcome</li>
-					<li>title</li>
-				</ul>
-
-				<h2 id="bill">Retrieve a given bill.</h2>
-
-				<h3>Example URI</h3>
-				<p>http://api.richmondsunlight.com/1.0/bill/2010/hb1.json</p>
-
-				<h3>Instructions</h3>
-				<p>Replace the year (“2010”) and bill number (“hb1”) with the bill number and year
-				in which it was introduced that you want.</p>
-
-				<h3>Returns</h3>
-				<ul>
-					<li>bill number</li>
-					<li>catch line</li>
-					<li>summary</li>
-					<li>full text</li>
-					<li>sponsor</li>
-					<li>outcome (passed/failed)</li>
-					<li>tags (if any)</li>
-				</ul>
-
-				<p>If no bill is found, this method will return a 404.</p>
+	<ul>
+		<li><a href="#bills">List of bills</a></li>
+		<li><a href="#bill">Bill detail</a></li>
+		<li><a href="#legislators">List of legislators</a></li>
+		<li><a href="#legislator">Legislator detail</a></li>
+		<li><a href="#vote">Vote detail</a></li>
+		<li><a href="#bysection">Bills by code section</a></li>
+		<li><a href="#section-video">Video clips by code section</a></li>
+		<li><a href="#videos">List of videos</a></li>
+		<li><a href="#video">Video detail</a></li>
+		<li><a href="#tag-suggest">Tag suggestions</a></li>
+	</ul>
 
 
-				<h2 id="legislator">Retrieve a given legislator.</h2>
+	<h2 id="bills">List of bills</h2>
 
-				<h3>Example URI</h3>
-				<p>http://api.richmondsunlight.com/1.0/legislator/rbbell.json</p>
+	<h3>Example URI</h3>
+	<p><code>https://api.richmondsunlight.com/1.1/bills/2025.json</code></p>
 
-				<h3>Instructions</h3>
-				<p>Replace the legislator (“rbbell”) with the Richmond Sunlight ID for that
-				legislator. (You can find each legislator’s ID in the URL for their page on the
-				site.)</p>
+	<h3>Instructions</h3>
+	<p>Replace the year with the four-digit session year for which you want a listing of
+	all bills.</p>
 
-				<h3>Returns</h3>
-				<ul>
-					<li>name</li>
-					<li>party</li>
-					<li>district number</li>
-					<li>district description</li>
-					<li>date took office</li>
-					<li>partisanship (0-100 scale; 0 == Democratic, 100 == Republican)</li>
-					<li>all bills (since 2006)
-						<ul>
-							<li>number</li>
-							<li>year</li>
-							<li>title</li>
-							<li>date introduced</li>
-							<li>outcome</li>
-							<li>Richmond Sunlight URL</li>
-						</ul>
-					</li>
-					<li>district office address</li>
-					<li>capitol office address</li>
-					<li>district office phone number</li>
-					<li>capitol office phone number</li>
-					<li>website URI (if any)</li>
-					<li>e-mail address</li>
-					<li>Virginia Public Access Project ID</li>
-					<li>Legislative Information System ID</li>
-				</ul>
-
-				<p>If no legislator is found, this method will return a 404.</p>
+	<h3>Returns</h3>
+	<ul>
+		<li>number</li>
+		<li>chamber</li>
+		<li>date introduced</li>
+		<li>status</li>
+		<li>outcome</li>
+		<li>title</li>
+		<li>patron (name and ID)</li>
+	</ul>
 
 
-				<h2 id="code-section">Retrieve a list of bills that affect a specific section of the
-				code.</h2>
+	<h2 id="bill">Bill detail</h2>
 
-				<h3>Example URI</h3>
-				<p>http://api.richmondsunlight.com/1.0/bysection/20-107.3.json</p>
+	<h3>Example URI</h3>
+	<p><code>https://api.richmondsunlight.com/1.1/bill/2025/hb1.json</code></p>
 
-				<h3>Instructions</h3>
-				<p>Replace the section number with the section that you’re interested in. It will return
-				a list of bills (from 2006 foreward) that cite that section number in the text of the
-				bill.</p>
+	<h3>Instructions</h3>
+	<p>Replace the year and bill number with the bill you want. Bill numbers must be
+	lowercase (e.g., <code>hb41</code>, <code>sb100</code>). Supported prefixes are
+	<code>hb</code> (House Bill), <code>sb</code> (Senate Bill), <code>hj</code>
+	(House Joint Resolution), <code>sj</code> (Senate Joint Resolution),
+	<code>hr</code> (House Resolution), and <code>sr</code> (Senate Resolution).</p>
 
-				<h3>Returns</h3>
-				<ul>
-					<li>bill number</li>
-					<li>year</li>
-					<li>catch line</li>
-					<li>summary</li>
-					<li>sponsor</li>
-					<li>outcome (passed/failed)</li>
-				</ul>
+	<h3>Returns</h3>
+	<ul>
+		<li>number</li>
+		<li>catch line</li>
+		<li>summary</li>
+		<li>status</li>
+		<li>outcome</li>
+		<li>patron</li>
+		<li>associated video clips (URL, screenshot, start/end time)</li>
+	</ul>
 
-				<p>If no bills are found, this method will return a 404.</p>
+	<p>Returns 404 if no bill is found.</p>
 
 
+	<h2 id="legislators">List of legislators</h2>
 
-				<h2 id="photosynthesis">Retrieve a list of bills and commentary on those bills for a
-				given Photosynthesis portfolio.</h2>
+	<h3>Example URI</h3>
+	<p><code>https://api.richmondsunlight.com/1.1/legislators.json</code></p>
+	<p><code>https://api.richmondsunlight.com/1.1/legislators.json?year=2025</code></p>
 
-				<h3>Example URI</h3>
-				<p>http://api.richmondsunlight.com/1.0/photosynthesis/7l0x3.json</p>
+	<h3>Instructions</h3>
+	<p>Returns all current legislators. Optionally filter by session year using the
+	<code>year</code> query parameter.</p>
 
-				<h3>Instructions</h3>
-				<p>Replace the portfolio number (“7l0x3”) with the number of the portfolio that
-				you’re interested in. (You can find this in the URL.) It will return a list of the
-				bills in that portfolio, along with any commentary that has been provided on each
-				of those bills.</p>
+	<h3>Returns</h3>
+	<ul>
+		<li>id</li>
+		<li>name</li>
+		<li>chamber (<code>house</code> or <code>senate</code>)</li>
+		<li>party</li>
+		<li>district</li>
+	</ul>
 
-				<h3>Returns</h3>
-				<ul>
-					<li>bill number</li>
-					<li>year</li>
-					<li>URL</li>
-					<li>notes</li>
-				</ul>
 
-				<p>If no bills is found for that portfolio, or if the portfolio doesn’t exist, this
-				method will return a 404.</p>
+	<h2 id="legislator">Legislator detail</h2>
+
+	<h3>Example URI</h3>
+	<p><code>https://api.richmondsunlight.com/1.1/legislator/rcdeeds.json</code></p>
+
+	<h3>Instructions</h3>
+	<p>Replace the shortname with the legislator's Richmond Sunlight identifier, which
+	appears in the URL on their page (e.g., <code>/legislator/rcdeeds/</code>).
+	Shortnames are lowercase and may contain hyphens.</p>
+
+	<h3>Returns</h3>
+	<ul>
+		<li>id</li>
+		<li>name</li>
+		<li>chamber</li>
+		<li>party</li>
+		<li>district</li>
+		<li>State Board of Elections ID (<code>sbe_id</code>)</li>
+		<li>all bills sponsored, with number, title, date introduced, outcome, and URL</li>
+	</ul>
+
+	<p>Returns 404 if no legislator is found.</p>
+
+
+	<h2 id="vote">Vote detail</h2>
+
+	<h3>Example URI</h3>
+	<p><code>https://api.richmondsunlight.com/1.1/vote/2025/1.json</code></p>
+
+	<h3>Instructions</h3>
+	<p>Replace the year and LIS vote ID number with the vote you want. The LIS ID is a
+	numeric identifier (1–6 digits) found in the vote URL on this site.</p>
+
+	<h3>Returns</h3>
+	<ul>
+		<li>chamber</li>
+		<li>outcome</li>
+		<li>tally</li>
+		<li>per-legislator record: name, shortname, vote, party, chamber, district, address, date started</li>
+	</ul>
+
+	<p>Returns 404 if no vote is found.</p>
+
+
+	<h2 id="bysection">Bills by code section</h2>
+
+	<h3>Example URI</h3>
+	<p><code>https://api.richmondsunlight.com/1.1/bysection/18.2-174.json</code></p>
+
+	<h3>Instructions</h3>
+	<p>Replace the section number (URL-encoded if it contains special characters) with
+	the Virginia Code section you want. Returns all bills from 2006 forward that cite
+	that section in their text.</p>
+
+	<h3>Returns</h3>
+	<ul>
+		<li>year</li>
+		<li>number</li>
+		<li>catch line</li>
+		<li>summary</li>
+		<li>outcome</li>
+		<li>chamber</li>
+		<li>legislator</li>
+	</ul>
+
+	<p>Returns 404 if no bills are found.</p>
+
+
+	<h2 id="section-video">Video clips by code section</h2>
+
+	<h3>Example URI</h3>
+	<p><code>https://api.richmondsunlight.com/1.1/section-video/18.2-174.json</code></p>
+
+	<h3>Instructions</h3>
+	<p>Replace the section number with the Virginia Code section you want. Returns video
+	clips from legislative proceedings that discuss that section.</p>
+
+	<h3>Returns</h3>
+	<ul>
+		<li>bill number</li>
+		<li>year</li>
+		<li>date</li>
+		<li>chamber</li>
+		<li>time start/end</li>
+		<li>screenshot URL</li>
+		<li>video URL</li>
+	</ul>
+
+	<p>Returns 404 if no clips are found.</p>
+
+
+	<h2 id="videos">List of videos</h2>
+
+	<h3>Example URI</h3>
+	<p><code>https://api.richmondsunlight.com/1.1/videos.json</code></p>
+	<p><code>https://api.richmondsunlight.com/1.1/videos.json?year=2025</code></p>
+
+	<h3>Instructions</h3>
+	<p>Returns all legislative videos. Optionally filter by year using the
+	<code>year</code> query parameter.</p>
+
+	<h3>Returns</h3>
+	<ul>
+		<li>id</li>
+		<li>date</li>
+		<li>chamber</li>
+		<li>committee</li>
+		<li>title</li>
+		<li>path</li>
+		<li>description</li>
+		<li>width/height</li>
+		<li>sponsor</li>
+		<li>has_transcript</li>
+		<li>is_indexed</li>
+	</ul>
+
+
+	<h2 id="video">Video detail</h2>
+
+	<h3>Example URI</h3>
+	<p><code>https://api.richmondsunlight.com/1.1/video/123.json</code></p>
+
+	<h3>Instructions</h3>
+	<p>Replace the numeric ID with the ID of the video you want.</p>
+
+	<h3>Returns</h3>
+	<p>Same fields as the videos list above.</p>
+
+	<p>Returns 404 if no video is found.</p>
+
+
+	<h2 id="tag-suggest">Tag suggestions</h2>
+
+	<h3>Example URI</h3>
+	<p><code>https://api.richmondsunlight.com/1.1/tag-suggest?term=crime</code></p>
+
+	<h3>Instructions</h3>
+	<p>Provide a partial tag string via the <code>term</code> query parameter. Returns
+	matching tag names for use in autocomplete or tag lookup.</p>
+
+	<h3>Returns</h3>
+	<p>Array of matching tag name strings (e.g., <code>["crime", "firearm",
+	"weapon"]</code>).</p>
+
+	<p>Returns 404 if no tags match.</p>
 
 EOD;
 
